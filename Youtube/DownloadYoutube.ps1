c05901016bf2e5c509b7ef89b7c192cb56d4b9d7
hoangnl <nguyenlehoang@hotmail.com>
2016-11-23 06:54:24 +0700

    Download playlist youtube

---
+++ Youtube/DownloadYoutube.ps1
@@ -0,0 +1,12 @@
+﻿$Path = "C:\Users\HOANG\Desktop\"
+$Playlisturl = "http://www.youtube.com/playlist?list=PL1058E06599CCF54D"
+$VideoUrls= (invoke-WebRequest -uri $Playlisturl).Links | ? {$_.HREF -like "/watch*"} | `
+? innerText -notmatch ".[0-9]:[0-9]." | ? {$_.innerText.Length -gt 3} | Select innerText, `
+@{Name="URL";Expression={'http://www.youtube.com' + $_.href}} | ? innerText -notlike "*Play all*"
+ 
+$VideoUrls
+ 
+ForEach ($video in $VideoUrls){
+    Write-Host ("Downloading " + $video.innerText)
+    C:\Users\HOANG\Desktop\Git\Powershell\youtube-dl.exe $video.URL --output ($Path + "%(title)s.%(ext)s")
+}
